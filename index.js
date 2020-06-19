63ab960b96dbca01327ed1e350c1a36ffcdafc17
Shyroun <mail@juliaalfeo.com>
2020-06-19 12:33:17 +0200

    update files

--- index.js
+++ index.js
@@ -4,35 +4,32 @@ const fetch = require("node-fetch");
 
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static("public"));
+console.log("boom1")
 
-/* app.get("/myAPI", async (request, response) => {
-    const api_url = "http://localhost:8080/api/swagger/?format=openapi";
+app.post("http://localhost:8080/api/v1/auth/login", async (request, response) => {
+    //console.log(request);
+    const api_url = "http://localhost:8080/api/v1/auth/login";
     const fetch_response = await fetch(api_url, {
-        headers: {
-            'Authorization': 'Basic ' + new Buffer('julia-alfeo:Q5zgxpGq').toString('base64'), 
-        }
-    });
-    console.log(fetch_response)
-    const json = await response.json();
-    response.json(json);
-}); */
 
-app.get("/myAPI", async (request, response) => {
-    const api_url = "http://localhost:8080/api/v1/jobs/5";
-    const fetch_response = await fetch(api_url, {
-        headers: {
-            'Authorization': 'Token 387f97608a6d057a0046d0be21eaa17272fca823',
-          }
     });
-    console.log(fetch_response)
     const json = await fetch_response.json();
     response.json(json);
     console.log(json)
 });
+/*
+ app.post("/myAPI", async (request, response) => {
+    console.log("boom2")
+    
 
+    
+    response.json(json);
+    console.log(json)
+    console.log(request);
+}); */
+/*
 app.post("/api", (request,response) => {
     console.log(request);
 
-});
+});*/
 
 // "key": "387f97608a6d057a0046d0be21eaa17272fca823"
